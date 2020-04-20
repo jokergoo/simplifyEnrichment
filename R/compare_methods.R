e1bed514a71553b621410f054832dd90b75b45f7
Zuguang Gu <jokergoo@gmail.com>
2020-04-21 00:07:13 +0200

    update

---
+++ R/compare_methods.R
@@ -0,0 +1,110 @@
+
+
+compare_methods_make_clusters = function(mat, method = "all") {
+
+	clt = list()
+	if(method == "all") {
+		method = c("kmeans", "dynamicTreeCut", "mclust", "apclust", "fast_greedy", "leading_eigen",
+			"louvain", "walktrap", "binary_cut")
+	}
+
+	clt = lapply(method, function(me) as.character(cluster_GO(mat, me)))
+	clt = as.data.frame(clt)
+
+	clt
+}
+
+compare_methods_make_plot = function(mat, clt) {
+
+	ht = Heatmap(mat, col = colorRamp2(c(0, 1), c("white", "red")),
+		name = "Similarity", column_title = "GO Similarity",
+		show_row_names = FALSE, show_column_names = FALSE, 
+		# cluster_rows = dend, cluster_columns = dend,
+		show_row_dend = FALSE, show_column_dend = FALSE,
+		right_annotation = rowAnnotation(df = clt, show_legend = FALSE))
+	p1 = grid.grabExpr(draw(ht))
+
+	x = sapply(clt, function(x) difference_score(mat, x))
+
+	if(!requireNamespace("ggplot2")) {
+		stop_wrap("Package ggplot2 should be installed.")
+	}
+	p2 = ggplot2::ggplot(NULL, ggplot2::aes(x = names(x), y = x)) + ggplot2::geom_bar(stat = "identity") +
+		ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 45, hjust = 1))
+
+	cm = compare_methods_calc_concordance(clt)
+	ht2 = Heatmap(cm, column_title = "concordance")
+	p3 = grid.grabExpr(draw(ht2))
+
+	if(!requireNamespace("cowplot")) {
+		stop_wrap("Package cowplot should be installed.")
+	}
+	print(cowplot::plot_grid(p1, cowplot::plot_grid(p2, p3, nrow = 1), nrow = 2))
+}
+
+# == title
+# Difference score
+#
+# == param
+# -mat The similarity matrix.
+# -cl Cluster labels.
+#
+difference_score = function(mat, cl) {
+	n = nrow(mat)
+	l_block = matrix(FALSE, nrow = nrow(mat), ncol = ncol(mat))
+
+	for(le in unique(cl)) {
+		l = cl == le
+		l_block[l, l] = TRUE
+	}
+	l_block2 = l_block
+	l_block2[upper.tri(mat)] = FALSE
+	x1 = mat[l_block2]
+
+	l_block2 = !l_block
+	l_block2[upper.tri(mat)] = FALSE
+	x2 = mat[l_block2]
+
+	ecdf1 = ecdf(x1)
+	ecdf2 = ecdf(x2)
+
+	p = seq(0, 1, length = 1000)
+	max(abs(ecdf2(p) - ecdf1(p)))
+}
+
+compare_methods_calc_concordance = function(clt) {
+
+	concordance = function(cl1, cl2) {
+		cl1 = as.vector(cl1)
+		cl2 = as.vector(cl2)
+		nle1 = length(unique(cl1))
+		nle2 = length(unique(cl2))
+
+		if(nle1 > nle2) {
+			cl2 = relabel_class(cl2, cl1, return_map = FALSE)
+		} else {
+			cl1 = relabel_class(cl1, cl2, return_map = FALSE)
+		}
+
+		sum(cl1 == cl2)/length(cl1)
+	}
+
+	n = length(clt)
+	mm = matrix(1, nrow = n, ncol = n)
+	rownames(mm) = names(clt)
+	colnames(mm) = names(clt)
+	for(i in 1:(n-1)) {
+		for(j in (i+1):n) {
+			mm[i, j] = concordance(clt[[i]], clt[[j]])
+			mm[j, i] = mm[i, j]
+		}
+	}
+
+	mm
+}
+
+compare_methods = function(mat, method = "all") {
+	clt = compare_methods_make_clusters(mat, method)
+	compare_methods_make_plot(mat, clt)
+}
+
---
+++ R/utils.R
@@ -0,0 +1,38 @@
+
+relabel_class = function (class, ref, full_set = union(class, ref), return_map = TRUE) {
+    if(!requireNamespace("clue")) {
+        stop_wrap("Package clue should be installed.")
+    }
+
+    md = mode(class)
+    class = as.character(class)
+    ref = as.character(ref)
+    full_set = as.character(full_set)
+    all_class = union(class, ref)
+    n = length(all_class)
+    m = matrix(0, nrow = n, ncol = n, dimnames = list(all_class, all_class))
+    tb = table(class, ref)
+    m[rownames(tb), colnames(tb)] = tb
+    imap = clue::solve_LSAP(m, maximum = TRUE)
+    map = structure(rownames(m)[imap], names = rownames(m))
+    map = map[unique(class)]
+    unmapped = setdiff(setdiff(full_set, class), names(map))
+    if (length(unmapped)) {
+        map = c(map, structure(unmapped, names = unmapped))
+    }
+    df = data.frame(class = class, adjusted = map[class], ref = ref, 
+        stringsAsFactors = FALSE)
+    attr(map, "df") = df
+    if (return_map) {
+        return(map)
+    }
+    else {
+        return(as(df$adjusted, md))
+    }
+}
+
+stop_wrap = function (...) {
+    x = paste0(...)
+    x = paste(strwrap(x), collapse = "\n")
+    stop(x, call. = FALSE)
+}
